3cae92ca3f588e2dbfaf8fa71dc9c934bff90231
dhiraj <d7765744@gmail.com>
2024-08-16 21:17:47 +0530

    java practice

---
+++ Java/Codechef.java
@@ -0,0 +1,73 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef{
+    static Scanner sc = new Scanner(System.in);
+    static boolean[] sieveOfEratosthenes(int limit){
+        boolean isprime[] = new boolean[limit+1];
+        Arrays.fill(isprime, true);
+        isprime[0]=false;
+        isprime[1]=false;
+        for (int i=2;i<=Math.sqrt(limit);i++){
+            if (isprime[i]){
+                for (int j=i*i;j<=limit;j+=i){
+                    isprime[j]=false;
+                }
+            }
+        }
+        return isprime;
+    }
+    
+    static void main() throws IOException{
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int a[] = new int[n];
+        
+        
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+            if (!map.containsKey(a[i])){
+                map.put(a[i],i);
+            }
+        }
+        if (n==k){
+            int sum = Arrays.stream(a).sum();
+            System.out.println(2*sum-a[0]-a[n-1]);
+            return;
+        }
+        int maxi=0;
+        for (int i=1;i<51;i++){
+            if (!map.containsKey(i)){
+                continue;
+            }
+            int ind = map.get(i);
+            PriorityQueue <Integer> pq = new PriorityQueue<>(Comparator.naturalOrder());
+            int sum=0;
+            for(int j=ind+1;j<n;j++){
+                if(pq.size()==(k-2)){
+                    maxi = Math.max(maxi,i+a[j]+sum);
+                }
+                pq.add(a[j]);
+                sum += 2*a[j];
+                if (pq.size() > (k-2)){
+                    sum -= 2*pq.poll();
+                }
+            }
+        }   
+        System.out.println(maxi);
+    }
+  
+    public static void main (String[] args) throws java.lang.Exception{
+
+        // Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        // boolean[] arr = sieveOfEratosthenes(201);
+        while (t-->0)
+        {
+            main();
+
+        }
+    }
+}
--- codechef/acc.py
+++ codechef/acc.py
@@ -1,26 +1,30 @@
 import math
 import sys
 input=sys.stdin.readline
-def main():
-    n,k=map(int,input().split())
-    s=input().strip()
-    if s[0]=='0':
-        print("NO")
-        return
-    val=0
+def main(arr):
+    n=int(input())
+    a=list(map(int,input().split()))
+    d={}
     for i in range(n):
-        if s[i]=='0':
-            if val<=0:
-                print("NO")
-                return
-            val-=1
-        elif s[i]=='1':
-            val=k
-        # if s[i]=='1':
-        #     val+=k
-        # else:
-        #     val-=1
-    # print(val)
-    print("YES" if val>=0 else "NO")
+        ele = a[i]
+        for j in range(ele+1,201):
+            if arr[j]==False:
+                diff = abs(a[i]-j)
+                if diff in d:
+                    print(i+1,d[diff]+1)
+                    return
+        d[ele]=i
+    print(-1)
+
+
+def sieve_of_eratosthenes(limit):
+    is_prime = [True] * (limit + 1)
+    is_prime[0] = is_prime[1] = False
+    for i in range(2, int(limit**0.5) + 1):
+        if is_prime[i]:
+            for j in range(i*i, limit + 1, i):
+                is_prime[j] = False
+    return is_prime
 for _ in range(int(input())):
-   main()
+   arr=sieve_of_eratosthenes(201)
+   main(arr)
